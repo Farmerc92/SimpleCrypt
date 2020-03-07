ad433372eb9b157cb4a0f4a60e7e3520466ba10a
Chris Farmer <farmerc@udel.edu>
2020-03-07 14:39:30 -0500

    added test to make sure sonnet18.txt and sonnet18.dec were identical

--- Crypto/src/test/java/com.zipcodewilmington.simplecrypt/FileWriterROT13Test.java
+++ Crypto/src/test/java/com.zipcodewilmington.simplecrypt/FileWriterROT13Test.java
@@ -1,9 +1,10 @@
 package com.zipcodewilmington.simplecrypt;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.*;
 
 public class FileWriterROT13Test {
 
@@ -17,6 +18,22 @@ public class FileWriterROT13Test {
 
     @Test
     public void testFileWriter(){
-
+        try {
+            BufferedReader reader1 = new BufferedReader(new FileReader("sonnet18.txt"));
+            BufferedReader reader2 = new BufferedReader(new FileReader("sonnet18.dec"));
+            String line1;
+            String line2;
+            while ((line1 = reader1.readLine()) != null && (line2 = reader2.readLine()) != null){
+                line1 = reader1.readLine();
+                line2 = reader2.readLine();
+                Assert.assertEquals(line1, line2);
+            }
+            reader1.close();
+            reader2.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
